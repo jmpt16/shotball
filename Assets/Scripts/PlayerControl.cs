dff2ba48f947e75bd72948acd67cecee05db24ee
João Carlos Soares Mendonça <A042438@ipmaia.pt>
2024-11-11 17:52:15 +0000

    back to testing

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 using Cinemachine;
 public class PlayerControl : MonoBehaviour
 {
-    public GameObject pivot, worldUp,antenaPivot;
+    public GameObject pivot,gunPivot;
     public Rigidbody rb;
     public float speed;
-    public float maxSpeed;
     float moveFwrd;
     float moveSide;
     float brake;
@@ -16,12 +15,7 @@ public class PlayerControl : MonoBehaviour
     public float brakeForce = 5f;
     public CinemachineFreeLook camSetup;
     Vector3 movement;
-    public AudioSource Trampolim;
-    public AudioSource Lançamento;
-    public AudioSource Gravidade;
-    public AudioSource Morte;
-    public AudioSource Botao;
-    public AudioSource Final;
+    bool colliding;
 
     void Start()
     {
@@ -33,29 +27,20 @@ public class PlayerControl : MonoBehaviour
     {
         //para garantir que o movimento não é dependente do angulo vertical (cima/baixo)
         pivot.transform.rotation = new Quaternion(0, Camera.main.transform.rotation.y, 0, Camera.main.transform.rotation.w);
+        gunPivot.transform.rotation = new Quaternion(Camera.main.transform.rotation.x, Camera.main.transform.rotation.y, Camera.main.transform.rotation.z, Camera.main.transform.rotation.w);
         //se gravidade virar, inverter antena e worldUp
-        if (Physics.gravity.y > 0)
-        {
-            antenaPivot.transform.rotation = new Quaternion(0, 0, 180, antenaPivot.transform.rotation.w);
-            worldUp.transform.rotation = new Quaternion(180, 0, 0, worldUp.transform.rotation.w);
-        }
-        else
-        {
-            antenaPivot.transform.rotation = new Quaternion(0, 0, 0, antenaPivot.transform.rotation.w);
-            worldUp.transform.rotation = new Quaternion(0, 0, 0, worldUp.transform.rotation.w);
-        }
         //input
         moveFwrd = Input.GetAxis("Horizontal");
         moveSide = Input.GetAxis("Vertical");
         brake = Input.GetAxis("Brake");
-
+        
     }
 
     void FixedUpdate()
     {
         // movimento
         movement = Camera.main.transform.right * moveFwrd + Camera.main.transform.forward * moveSide;
-        movement.y = rb.velocity.y;
+        //movement.y = rb.velocity.y;
         movement = movement.normalized;
 
         if (brake > 0)
@@ -66,39 +51,11 @@ public class PlayerControl : MonoBehaviour
         Vector3 horizontalSpeed = new Vector3(rb.velocity.x, 0, rb.velocity.z);
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.tag == "Trampolim")
+        if (Input.GetAxis("Jump") > 0)
         {
-            Trampolim.Play();
-        }
-
-        if (collision.gameObject.tag == "Gravidade")
-        {
-            Gravidade.Play();
-        }
-
-        if (collision.gameObject.tag == "Morte")
-        {
-            Morte.Play();
-        }
-
-        if (collision.gameObject.tag == "Botao")
-        {
-            Botao.Play();
-        }
-
-        if (collision.gameObject.tag == "Final")
-        {
-            Final.Play();
-        }
-    }
-
-    void OnTriggerEnter(Collider collider)
-    {
-        if (collider.gameObject.tag == "Lançamento")
-        {
-            Lançamento.Play();
+            rb.velocity += Vector3.up * jumpSpeed;
         }
     }
 }
